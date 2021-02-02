ab2c7e5c448d9218db6ab08a29a7b2a0176aa1e9
Rita <rita@ritacorreia.co>
2021-02-02 17:20:20 +0000

    Hero component design

--- components/Hero.js
+++ components/Hero.js
@@ -3,20 +3,14 @@ import styles from "./hero.module.css";
 export default function Hero() {
 	return (
 		<>
-			{" "}
+			<div class={styles.bg} aria-hidden="true"></div>
 			<div class={styles.container}>
 				<h1 class={styles.title}>
-					Welcome to <span class={styles.bright}>Bright.</span>
+					Welcome to <br />
+					<span class={styles.bright}>Bright.</span>
 				</h1>
 
-				<p class={styles.description}>Your curated dev job board</p>
-
-				<img
-					class={styles.bg}
-					src="../assets/triangle1.svg"
-					aria-hidden="true"
-					alt=" "
-				/>
+				<p class={styles.description}>Your curated dev job board.</p>
 			</div>
 		</>
 	);
