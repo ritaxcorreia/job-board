da107b03fa4f079754c3ae34a94347dccc98a64f
Rita <rita@ritacorreia.co>
2021-02-08 14:01:22 +0000

    Added favicon

--- pages/index.js
+++ pages/index.js
@@ -15,35 +15,27 @@ export default function Home() {
 		<div className={styles.container}>
 			<Head>
 				<title>Bright.</title>
-				<link rel="icon" href="/favicon.ico" />
 
-				{/* <link
+				<link
 					rel="apple-touch-icon"
 					sizes="180x180"
-					href="/favicon/apple-touch-icon.png"
+					href="/assets/apple-touch-icon.png"
 				/>
 				<link
 					rel="icon"
 					type="image/png"
 					sizes="32x32"
-					href="/favicon/favicon-32x32.png"
+					href="/assets/favicon-32x32.png"
 				/>
 				<link
 					rel="icon"
 					type="image/png"
 					sizes="16x16"
-					href="/favicon/favicon-16x16.png"
+					href="/assets/favicon-16x16.png"
 				/>
-				<link rel="manifest" href="/favicon/site.webmanifest" />
-				<link
-					rel="mask-icon"
-					href="/favicon/safari-pinned-tab.svg"
-					color="#5bbad5"
-				/>
-				<meta name="msapplication-TileColor" content="#da532c" />
+				<link rel="manifest" href="/site.webmanifest" />
+				<meta name="msapplication-TileColor" content="#000000" />
 				<meta name="theme-color" content="#ffffff" />
-
-				<link rel="preconnect" href="https://fonts.gstatic.com" /> */}
 
 				<meta
 					id="meta"
@@ -77,7 +69,7 @@ export default function Home() {
 
 			<main className={styles.main}>
 				<Hero />
-				{/* <Form /> */}
+				<Form />
 
 				<ExploreCities />
 			</main>
